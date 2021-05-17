964e11397b310bd6c28b69db7e4aacb63af548d1
SophieHarding <sophiejaneharding@outlook.com>
2021-05-17 17:20:53 +1200

    updates

--- Week 11/289.101.11.files/collision_detection/script.js
+++ Week 11/289.101.11.files/collision_detection/script.js
@@ -13,18 +13,55 @@ Next, add the following features:
 var puck = document.getElementById('puck');
 var puck_x = 210;
 var puck_y = 210;
+var speed = 50;
+
+function logcoordinates() {
+   console.log('x=' + puck_x + ' y=' + puck_y);
+}
+
+
+function checkCollision() {
+    //detect collision
+    if(puck_x >= 250 && puck_x <= 350){
+        
+        if(puck_y >= 150 && puck_y <=250){
+            console.log('collision!!')
+        }
+        
+    }
+    console.log('check');
+}
+
 puck.style.top = puck_y + 'px';
 puck.style.left = puck_x + 'px';
+
 
 document.getElementById('puck').style.top = puck_y + 'px';
 document.getElementById('puck').style.left = puck_x + 'px';
 
 document.getElementById('left').addEventListener('click', () => {
-puck_x -= 50;
-document.getElementById('puck').style.left = puck_x + 'px';
+puck_x -= speed;
+puck.style.left = puck_x + 'px';
+    logcoordinates();
+    checkCollision();
+});
+document.getElementById('right').addEventListener('click', () => {
+puck_x += speed;
+puck.style.left = puck_x + 'px';
+    logcoordinates();
+    checkCollision();
 });
 
-document.getElementById('right').addEventListener('click', () => {
-puck_x += 50;
-document.getElementById('puck').style.left = puck_x + 'px';
+document.getElementById('up').addEventListener('click', () => {
+puck_x -= speed;
+document.getElementById('puck').style.top = puck_x + 'px';
+    logcoordinates();
+    checkCollision();
+});
+
+document.getElementById('down').addEventListener('click', () => {
+puck_x += speed;
+document.getElementById('puck').style.top = puck_x + 'px';
+    logcoordinates();
+    checkCollision();
 });
